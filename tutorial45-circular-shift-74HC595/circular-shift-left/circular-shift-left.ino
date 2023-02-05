58fb11c81eb312d5964f3451f41fc0c1d6220def
Dhruv Mohindru <dmohindru@gmail.com>
2023-02-05 11:29:49 +1100

    Added code example for tutorial 44, 45, 46,47

---
+++ tutorial45-circular-shift-74HC595/circular-shift-left/circular-shift-left.ino
@@ -0,0 +1,28 @@
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+byte LEDs=0b11110000;
+
+int dt = 2000;
+void setup() {
+  Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+
+}
+
+void loop() {
+
+  // set latchPin low to trigger data incoming
+  digitalWrite(latchPin, LOW);
+  // LSBFIRST makes Q7 pin as LSB and Q0 as MSB
+  // MSBFIRST makes Q0 pin as LSB and Q7 as MSB
+  shiftOut(dataPin, clockPin, LSBFIRST, LEDs);
+  digitalWrite(latchPin, HIGH);
+  LEDs = (LEDs << 1 | LEDs >> 7);
+  delay(dt);
+
+  
+
+}
